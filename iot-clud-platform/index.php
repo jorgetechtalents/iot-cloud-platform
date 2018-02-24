09186cf6523c32f797e1d416604eac79c49f7a25
jorgetechtalents <33369864+jorgetechtalents@users.noreply.github.com>
2018-02-24 13:33:00 +0100

    basic steucture setup
    iot cloud platform

---
+++ iot-clud-platform/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Clud Platform</title>
+    <link rel ="stylesheet" href="styles/style.css">
+    <script src="scripts/script.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
